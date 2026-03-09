363e41515dd018d8bbf7f384d83e9bb4d3e5ca3c
armantorkzaban <armantorkzaban@icloud.com>
2026-03-09 10:05:29 +0100

    refactor: improve code formatting and readability in HeroBanner component

--- src/pages/Homepage/components/HeroBanner.vue
+++ src/pages/Homepage/components/HeroBanner.vue
@@ -45,20 +45,32 @@
         <!--        </template>-->
 
         <div class="hero-banner__desc-links">
-          <router-link v-for="(item, idx) in appLinks" :key="idx" class="hero-banner__desc-links-item" :to="{
-            name: item.routeName,
-            params: {
-              deviceType: item.deviceType,
-            },
-          }">
-            <icon class="hero-banner__desc-links-item-icon" :style="{
-              ...(item?.iconColor && { color: item?.iconColor }),
-            }" :name="item.iconName" />
+          <router-link
+            v-for="(item, idx) in appLinks"
+            :key="idx"
+            class="hero-banner__desc-links-item"
+            :to="{
+              name: item.routeName,
+              params: {
+                deviceType: item.deviceType,
+              },
+            }"
+          >
+            <icon
+              class="hero-banner__desc-links-item-icon"
+              :style="{
+                ...(item?.iconColor && { color: item?.iconColor }),
+              }"
+              :name="item.iconName"
+            />
             <span class="hero-banner__desc-links-item-text">
               {{ item.text }}
             </span>
 
-            <app-button class="hero-banner__desc-links-item-btn" :text="$t('hero-banner.vote-link')" />
+            <app-button
+              class="hero-banner__desc-links-item-btn"
+              :text="$t('hero-banner.vote-link')"
+            />
           </router-link>
         </div>
       </div>
